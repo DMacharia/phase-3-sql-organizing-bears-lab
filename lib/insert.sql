a6cfdf246bd2e1b077c81470cfc42bfcd76ba675
Daniel Macharia <danielhuios@gmail.com>
2022-11-01 22:19:59 +0300

    bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Chocolate",2,"F","Brown","cute","TRUE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Choco",3,"F","Black","cute","TRUE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Chocola",1,"M","Brown","cute","FALSE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Cho",4,"F","Grey","cute","TRUE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Chocula",2,"M","Grey","cute","TRUE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Chocs",1,"F","Brown","cute","FALSE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values ("Mr.Chocolat",3,"M","White","cute","TRUE");
+INSERT INTO bears(name,age,sex,color,temperament,alive) values (NULL, 2,"F","Brown","cute","TRUE");
